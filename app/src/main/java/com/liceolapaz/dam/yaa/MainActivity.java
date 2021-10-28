3078bced74ffe433f8e73e9db086a78e4f0140c0
yagofingoi1234@gmail.com <yagofingoi1234@gmail.com>
2021-10-28 10:20:54 +0200

    Añadiendo el login y su fincionalidad

--- app/src/main/java/com/liceolapaz/dam/yaa/MainActivity.java
+++ app/src/main/java/com/liceolapaz/dam/yaa/MainActivity.java
@@ -2,13 +2,53 @@ package com.liceolapaz.dam.yaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    //Declaración de variables
+    EditText usuario, contraseña;
+    Button btn_login;
+    int contador = 3; //Contador de intentos
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Relacionar con los id de los campos y botón
+        usuario = findViewById(R.id.usuario);
+        contraseña = findViewById(R.id.contraseña);
+        btn_login = findViewById(R.id.btn_login);
+
+        //Cuando pulsas Login
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (usuario.getText().toString().equals("admin") && contraseña.getText().toString().equals("liceo")) { //Cuando se cumplan estas condiciones
+                    //Paso a la siguiente Activity
+                    Intent intent = new Intent(MainActivity.this, MostrarUsuarios.class);
+                    startActivity(intent);
+
+                } else {
+                    contador--;
+                    Toast.makeText(MainActivity.this, "Usuario y/o contraseña incorrectos, te queda " +contador+ " intentos", Toast.LENGTH_LONG).show(); //Cuando no se cumplan
+
+                    if (contador == 0) { //Cuando el contador llege a 0:
+                        System.exit(0);//Se sale de la aplicación
+                    }
+
+                }
+
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/liceolapaz/dam/yaa/MostrarUsuarios.java
@@ -0,0 +1,14 @@
+package com.liceolapaz.dam.yaa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarUsuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_usuarios);
+    }
+}
